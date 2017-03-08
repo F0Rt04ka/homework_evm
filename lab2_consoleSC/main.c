37b8b9e598fb5d7d785a32601489e4703c512007
F0Rt04ka <rmn.2007@mail.ru>
2017-03-08 12:48:41 +0700

    completed lab2

--- lab2_consoleSC/main.c
+++ lab2_consoleSC/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
+    mt_clrscr();
+    mt_gotoXY(10, 5);
+    mt_setfgcolor(2);
+    mt_setbgcolor(3);
+    
     printf("Hello, World!\n");
+    mt_setdefaultcolors();
     return 0;
 }
