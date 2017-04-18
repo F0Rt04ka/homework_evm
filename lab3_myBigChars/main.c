c7d05901015ff2bf67faa0402ae5a295a1e0965c
Roman <rmn.2007@mail.ru>
2017-04-18 23:29:56 +0700

    Готовая 3 лаба

--- lab3_myBigChars/main.c
+++ lab3_myBigChars/main.c
@@ -1,81 +1,87 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "myBigChars.h"
 #include "../lab2_consoleSC/myTerms.h"
 
-#define MEM "Memory"
+void myPrint(char* string);
 
 int main()
 {
     int i;
-    char buf[6];
+    char* buf;
 
     mt_clrscr();
 
     bc_box(1, 1, 61, 12);
     mt_gotoXY(25, 1);
-    strcpy(buf, MEM);
-    write(1, buf, 6);
-//
-//    bc_box(62, 1, 80, 3);
-//    mt_gotoXY(66, 1);
-//    printf("Accumulator");
-//
-//    bc_box(62, 4, 80, 6);
-//    mt_gotoXY(62, 4);
-//    printf("instructionCounter");
-//
-//    bc_box(62, 7, 80, 9);
-//    mt_gotoXY(66, 7);
-//    printf("Operation");
-//
-//    bc_box(62, 10, 80, 12);
-//    mt_gotoXY(68, 10);
-//    printf("Flags");
-//
-//    bc_box(1, 13, 50, 24);
-//
-//    bc_box(51, 13, 80, 24);
-//    mt_gotoXY(53, 13);
-//    printf("Keys");
-//
-//    mt_gotoXY(52, 14);
-//    printf("l  - load");
-//    mt_gotoXY(52, 15);
-//    printf("s  - save");
-//    mt_gotoXY(52, 16);
-//    printf("r  - run");
-//    mt_gotoXY(52, 17);
-//    printf("t  - step");
-//    mt_gotoXY(52, 18);
-//    printf("i  - reset");
-//    mt_gotoXY(52, 19);
-//    printf("F5 - accumulator");
-//    mt_gotoXY(52, 20);
-//    printf("F6 - instructionCounter");
-//
-//    for (i = 0; i < 100; ++i) {
-//        mt_gotoXY(2 + ((i % 10) * 6), (i / 10) + 2);
-//        printf("+0000");
-//    }
-//
-//    int bigPlus[2] = BIG_PLUS;
-//    bc_printbigchar(bigPlus, 2, 14, Black, Default);
-//    int big9[2] = BIG_9;
-//    bc_printbigchar(big9, 11, 14, Default, Default);
-//    bc_printbigchar(big9, 20, 14, Default, Default);
-//    bc_printbigchar(big9, 29, 14, Default, Default);
-//    bc_printbigchar(big9, 38, 14, Default, Default);
-//
-//    mt_gotoXY(20, 4);
-//    mt_setbgcolor(Red);
-//    mt_setfgcolor(White);
-//    printf("+9999");
-//
-//    mt_setbgcolor(Default);
-//    mt_setfgcolor(Default);
+    myPrint("Memory");
+
+    bc_box(62, 1, 80, 3);
+    mt_gotoXY(66, 1);
+    myPrint("Accumulator");
+
+    bc_box(62, 4, 80, 6);
+    mt_gotoXY(62, 4);
+    myPrint("instructionCounter");
+
+    bc_box(62, 7, 80, 9);
+    mt_gotoXY(66, 7);
+    myPrint("Operation");
+
+    bc_box(62, 10, 80, 12);
+    mt_gotoXY(68, 10);
+    myPrint("Flags");
+
+    bc_box(1, 13, 50, 24);
+
+    bc_box(51, 13, 80, 24);
+    mt_gotoXY(53, 13);
+    myPrint("Keys");
+
+    mt_gotoXY(52, 14);
+    myPrint("l  - load");
+    mt_gotoXY(52, 15);
+    myPrint("s  - save");
+    mt_gotoXY(52, 16);
+    myPrint("r  - run");
+    mt_gotoXY(52, 17);
+    myPrint("t  - step");
+    mt_gotoXY(52, 18);
+    myPrint("i  - reset");
+    mt_gotoXY(52, 19);
+    myPrint("F5 - accumulator");
+    mt_gotoXY(52, 20);
+    myPrint("F6 - instructionCounter");
+
+    for (i = 0; i < 100; ++i) {
+        mt_gotoXY(2 + ((i % 10) * 6), (i / 10) + 2);
+        myPrint("+0000");
+    }
+
+    int bigPlus[2] = BIG_PLUS;
+    bc_printbigchar(bigPlus, 2, 14, Black, Default);
+    int big9[2] = BIG_9;
+    bc_printbigchar(big9, 11, 14, Default, Default);
+    bc_printbigchar(big9, 20, 14, Default, Default);
+    bc_printbigchar(big9, 29, 14, Default, Default);
+    bc_printbigchar(big9, 38, 14, Default, Default);
+
+    mt_gotoXY(20, 4);
+    mt_setbgcolor(Red);
+    mt_setfgcolor(White);
+    myPrint("+9999");
+
+    mt_setbgcolor(Default);
+    mt_setfgcolor(Default);
     mt_gotoXY(1, 25);
 
     return 0;
+}
+
+void myPrint(char* string)
+{
+    write(1, string, strlen(string));
 }
--- lab3_myBigChars/myBigChars.c
+++
@@ -1,9 +0,0 @@
-﻿#include "myBigChars.h"
-
-#include "bc_printA.c"
-#include "bc_box.c"
-#include "bc_printbigchar.c"
-#include "bc_setbigcharpos.c"
-#include "bc_getbigcharpos.c"
-#include "bc_bigcharwrite.c"
-#include "bc_bigcharread.c"
